f8d646668308bccba18cae99f7af11e1a9047ae9
Kongkon-79 <kongkonjowarder79@gmail.com>
2026-02-01 13:04:14 +0600

    what do we offer section image problem fixup

--- src/app/(website)/_components/what-do-we-offer.tsx
+++ src/app/(website)/_components/what-do-we-offer.tsx
@@ -6,9 +6,9 @@ const WhatDoWeOffer = () => {
     return (
         <div className='py-6 md:py-12 lg:py-16'>
 
-            <div className='container grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-12 xl:gap-24'>
+            <div className='container grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-20 lg:gap-32 xl:gap-48'>
                 <div className="order-2 md:order-1 md:col-span-1 flex items-center">
-                    <div className=' bg_color shadow-[0px_4px_16px_0px_#00000029] rounded-[16px] flex flex-col justify-center py-6 md:py-10 lg:py-14 px-6 md:px-10 xl:px-20'>
+                    <div className=' bg_color shadow-[0px_4px_16px_0px_#00000029] rounded-[16px] flex flex-col justify-center py-6 md:py-10 lg:py-14 px-6 md:px-10 xl:px-14'>
                     <h3 className='text-2xl md:text-3xl lg:text-4xl text-[#131313] h_underline leading-[120%] font-normal'>What do we offer</h3>
 
                     <ul className='py-6 md:py-10 lg:py-12 list-disc list-inside'>
