be1d1ddbeb6d6f083f78cf2d2e2841d004b52d4f
Kongkon-79 <kongkonjowarder79@gmail.com>
2026-02-09 11:22:18 +0600

    problem fix up

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image'
 import React, { Suspense } from 'react'
 import LoginForm from './_components/login-form'
-const SignupPage = () => {
+const LoginPage = () => {
     return (
         <div className='w-full min-h-screen grid grid-cols-1 md:grid-cols-2'>
             <div className='md:col-span-1'>
-                <Image src="/assets/images/auth.jpeg" alt="Auth Image" width={1000} height={1000} className='object-cover w-full h-screen' />
+                <Image src="/assets/images/auth_image.jpeg" alt="Auth Image" width={1000} height={1000} className='object-cover w-full h-screen' />
             </div>
             <div className='md:col-span-1 flex items-center justify-center'>
                 <Suspense fallback={<div>Loading...</div>}>
@@ -14,4 +14,4 @@ const SignupPage = () => {
             </div>
         </div>)
 }
-export default SignupPage
+export default LoginPage
